39dbb628b7ffba29bc5fc873e81182f0963bbe77
Ori31 <shpersonal000@gmail.com>
2026-07-20 10:15:45 -0600

    feat: First Commit

---
+++ src/main/java/com/mx/edu/tecdesoftware/PlataformaStreaming/PlataformaStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.mx.edu.tecdesoftware.PlataformaStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/edu/tecdesoftware/PlataformaStreaming/PlataformaStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.edu.tecdesoftware.PlataformaStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
