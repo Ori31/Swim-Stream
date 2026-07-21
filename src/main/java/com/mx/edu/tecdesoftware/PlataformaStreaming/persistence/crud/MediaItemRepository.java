ce45d15fc6a109e3ba62c38cba4d74286b125e38
Ori31 <shpersonal000@gmail.com>
2026-07-21 10:43:31 -0600

    feat: add @Repository in MediaRequestRepository and MediaItemRepository

--- src/main/java/com/mx/edu/tecdesoftware/PlataformaStreaming/persistence/crud/MediaItemRepository.java
+++ src/main/java/com/mx/edu/tecdesoftware/PlataformaStreaming/persistence/crud/MediaItemRepository.java
@@ -2,9 +2,11 @@ package com.mx.edu.tecdesoftware.PlataformaStreaming.persistence.crud;
 
 import com.mx.edu.tecdesoftware.PlataformaStreaming.persistence.entity.MediaItem;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MediaItemRepository extends CrudRepository<MediaItem, Integer> {
 
     List<MediaItem> findByMediaType(String mediaType);
--- src/main/java/com/mx/edu/tecdesoftware/PlataformaStreaming/persistence/crud/MediaRequestRepository.java
+++ src/main/java/com/mx/edu/tecdesoftware/PlataformaStreaming/persistence/crud/MediaRequestRepository.java
@@ -2,9 +2,11 @@ package com.mx.edu.tecdesoftware.PlataformaStreaming.persistence.crud;
 
 import com.mx.edu.tecdesoftware.PlataformaStreaming.persistence.entity.MediaRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MediaRequestRepository extends CrudRepository<MediaRequest, Integer> {
 
     List<MediaRequest> findByStatus(String status);
